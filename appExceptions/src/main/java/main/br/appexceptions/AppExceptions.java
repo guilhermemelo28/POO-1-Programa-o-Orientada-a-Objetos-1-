3685efbea83b6a308eb2b67f441b8198cfc26e03
guilhermemelo28 <guimelomoura28@gmail.com>
2026-09-15 08:12:48 -0300

    Adiciona a pasta appExceptions

---
+++ appExceptions/src/main/java/main/br/appexceptions/AppExceptions.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.br.appexceptions;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class AppExceptions {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando a main");
+        Exceptions teste01 = new Exceptions();
+        //teste01.exemploExcecao();
+        //try {    
+           // teste01.throws01();
+        //} catch (Exception ex) {
+          //  System.out.println("Formato de idade invalido :( !");
+        //}
+        //System.out.println("Saindo da main");
+        
+        
+        teste01.lerarquivoTXT();
+    }
+}
---
+++ appExceptions/src/main/java/main/br/appexceptions/Exceptions.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.br.appexceptions;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Exceptions {
+    public void exemploExcecao(){
+    
+    int numer[] = {4, 8, 16, 32, 65, 128,256};
+    int denom[] = {2, 0 , 4 ,4 ,0 ,8};
+    
+    for(int i = 0; i < numer.length; i++)
+        {
+          try{
+              System.out.println(numer[i]+ "/"+ denom[i] + 
+              "eh" + numer[i] / denom[i]);
+              
+            }
+          catch(ArithmeticException ex){
+              System.out.println("Erro ao dividir por zero!");
+            }
+          catch(ArrayIndexOutOfBoundsException ex){
+              System.out.println("Posicao do vetor inexistente!");
+          }
+          
+        }
+       
+    }
+        
+    public void throws01() throws Exception{
+        //try {
+            System.out.println("Inicio Metodo 1");
+            //Chamando metodo 2 para gerar ENCADEAMENTO
+            throws02();
+        //} catch (Exception ex) {
+            //System.out.println("Erro de formato de dado");
+        //}
+        
+        System.out.println("Fim do metodo 1");
+    }
+    
+    public void throws02() throws Exception{
+        System.out.println("Inicio metodo 2");
+        Integer idade = 0;
+        idade = Integer.parseInt(JOptionPane.showInputDialog("Digite"
+        + "uma idade:"));
+        System.out.println("Idade informada:"   +idade);
+        System.out.println("Fim Metodo 2");
+        
+    }
+    
+    
+    public void lerarquivoTXT(){
+            BufferedReader reader = null;
+        try {
+            String file = "fileTest.txt";
+            reader = new BufferedReader (new FileReader (file));
+            String currentLine = reader.readLine();
+            System.out.println("Dados"+currentLine);
+                    
+        } catch (FileNotFoundException ex) {
+            System.out.println("Arquivo nao encontrado");
+        } catch (IOException ex) {
+            System.out.println("Erro ao acessar o arquivo");
+        }
+        finally{
+            if(reader != null)
+            {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    System.out.println("Erro ao fechar o arquivo");
+                }
+            }
+        }
+        
+        
+            
+    }
+}
